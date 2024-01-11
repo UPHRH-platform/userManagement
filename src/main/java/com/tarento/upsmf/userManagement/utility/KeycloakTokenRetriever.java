4f435f091c0cf1144bd102c586213334757a2854
Mahesh Maney R <mahesh-maney@users.noreply.github.com>
2024-01-11 20:24:59 +0530

    adding token with password granttype.

--- src/main/java/com/tarento/upsmf/userManagement/utility/KeycloakTokenRetriever.java
+++ src/main/java/com/tarento/upsmf/userManagement/utility/KeycloakTokenRetriever.java
@@ -75,4 +75,31 @@ public class KeycloakTokenRetriever {
 
         return jsonNode;
     }
+
+    public JsonNode getAdminTokenRead() throws IOException {
+        String tokenEndpoint = ADMIN_TOKEN_ENDPOINT;
+        logger.info("Token endpoint: {}" ,tokenEndpoint);
+
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(tokenEndpoint);
+        httpPost.setHeader(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded");
+
+        String requestBody = "username=" + ADMIN_USERNAME +
+                "&password=" + ADMIN_PASSWORD +
+                "&grant_type=password" +
+                "&client_id=admin-cli" +
+                "&client_secret=" + ADMIN_TOKEN_SECRET;
+        logger.info("Request body: {}", requestBody);
+
+        StringEntity entity = new StringEntity(requestBody);
+        httpPost.setEntity(entity);
+
+        org.apache.http.HttpResponse response = httpClient.execute(httpPost);
+        String responseBody = EntityUtils.toString(response.getEntity());
+        JsonNode jsonNode = mapper.readTree(responseBody);
+
+        return jsonNode;
+    }
+
+
 }
